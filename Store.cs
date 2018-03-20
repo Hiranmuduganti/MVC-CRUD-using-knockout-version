b50ecfdbb6f04784592edda21c44f43fc50cf058
Hiran <34158740+Hiranmuduganti@users.noreply.github.com>
2018-03-20 13:13:18 +1100

    Add files via upload

---
+++ Store.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.ComponentModel.DataAnnotations.Schema;
+
+
+namespace mvcapplication.Models
+{
+    public class Store
+
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+    }
+}
